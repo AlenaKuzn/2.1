9a69314dfbd0ea265a0aa2a2eedce4eb62db10d5
AlenaKuzn <99790256+AlenaKuzn@users.noreply.github.com>
2022-04-26 14:59:41 +0300

    numbers.py

---
+++ numbers.py
@@ -0,0 +1,9 @@
+print("Enter four numbers ")
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+summa1 = n1 + n2
+summa2 = n3 + n4
+result = summa1 / summa2
+print("result", format(result, ".2"))
